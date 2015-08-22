92f033d3a81f65d7c88c4a1b3fad3a6fef58dfe3
Areg Ghazaryan <ghareg@gmail.com>
2015-08-21 22:23:34 -0500

    Made some corrections in ABCore class

--- ABCore.cpp
+++ ABCore.cpp
@@ -61,15 +61,33 @@ int16_t ABCore::alpha_beta_iterative(PositionState& pos, uint16_t depth, int16_t
 	uint16_t current_depth = 0;
 	int16_t current_alpha = alpha;
 	int16_t current_beta = beta;
+	uint16_t try_count = 0;
 	while (current_depth <= depth) {
 		score = alpha_beta(pos, current_depth, current_alpha, current_beta, white_to_play);
-		if (score <= current_alpha || score >= current_beta) {
-			current_alpha = -MAX_SCORE;
-			current_beta = MAX_SCORE;
+		if (score <= current_alpha) {
+			if (try_count < MAX_TRY) {
+				current_alpha -= (try_count + 1) * DELTA;
+				++try_count;
+			}
+			else {
+				current_alpha = -MAX_SCORE;
+			}
+		}
+		else if (score >= current_beta) {
+			if (try_count < MAX_TRY) {
+				current_beta += (try_count + 1) * DELTA;
+				++try_count;
+			}
+			else {
+				current_beta = MAX_SCORE;
+			}
 		}
 		else {
-			current_alpha = score - ASP_WINDOW;
-			current_beta = score + ASP_WINDOW;
+			if (current_depth >= ASP_DEPTH) { 
+				current_alpha = score - ASP_WINDOW;
+				current_beta = score + ASP_WINDOW;
+			}
+			try_count = 0;
 			++current_depth;
 		}
 	}
@@ -79,16 +97,8 @@ int16_t ABCore::alpha_beta_iterative(PositionState& pos, uint16_t depth, int16_t
 
 int16_t ABCore::alpha_beta(PositionState& pos, uint16_t depth, int16_t alpha, int16_t beta, bool white_to_play)
 {
-	eval_info eval;
-	if (_trans_table->contains(pos, eval) && eval.depth >= depth) {
-		return eval.pos_value;
-	}
-
 	if (depth == 0) {
-		int16_t score = quiescence_search(pos, alpha, beta, white_to_play);
-		eval_info eval(score, pos.get_zob_key(), depth);
-		_trans_table->force_push(eval);
-		return score;
+		return quiescence_search(pos, depth, alpha, beta, white_to_play);
 	}
 
 	moves_array& possible_moves = MemPool::instance()->get_moves_array(depth);
@@ -134,18 +144,29 @@ int16_t ABCore::alpha_beta(PositionState& pos, uint16_t depth, int16_t alpha, in
 		}
 	}
 	
-	eval.pos_value = score;
-	eval.depth = depth;
-	eval.zob_key = pos.get_zob_key();
-	_trans_table->force_push(eval);
-
 	return score;
 }
 
-int16_t ABCore::quiescence_search(PositionState& pos, int16_t alpha, int16_t beta, bool white_to_play)
+int16_t ABCore::quiescence_search(PositionState& pos, int16_t qs_depth, int16_t alpha, int16_t beta, bool white_to_play)
 {
 	//TODO : Consider check evasions
-	int16_t val = _pos_eval->evaluate(pos);
+	eval_info eval;
+	int16_t val;
+	if (_trans_table->contains(pos, eval) && eval.depth >= 0) {
+		val = eval.pos_value;
+	}
+	else {
+		val = _pos_eval->evaluate(pos);
+		eval.pos_value = val;
+		eval.zob_key = pos.get_zob_key();
+		eval.depth = 0;
+		_trans_table->force_push(eval);
+	}
+
+	if (qs_depth == -MAX_QUIET_DEPTH) {
+		return val;
+	}
+
 	int16_t current_alpha = alpha;
 	int16_t current_beta = beta;
 	if (white_to_play) {	
@@ -161,7 +182,7 @@ int16_t ABCore::quiescence_search(PositionState& pos, int16_t alpha, int16_t bet
 		int16_t score;
 		for (uint16_t i = 0; i < possible_moves.size(); ++i) {
 			pos.make_move(possible_moves[i]);
-			score = quiescence_search(pos, current_alpha, current_beta, !white_to_play);
+			score = quiescence_search(pos, qs_depth - 1, current_alpha, current_beta, !white_to_play);
 			pos.undo_move();
 			if (score > current_alpha) {
 				current_alpha = score;
@@ -185,7 +206,7 @@ int16_t ABCore::quiescence_search(PositionState& pos, int16_t alpha, int16_t bet
 		int16_t score;
 		for (uint16_t i = 0; i < possible_moves.size(); ++i) {
 			pos.make_move(possible_moves[i]);
-			score = quiescence_search(pos, current_alpha, current_beta, !white_to_play);
+			score = quiescence_search(pos, qs_depth - 1, current_alpha, current_beta, !white_to_play);
 			pos.undo_move();
 			if (score < current_beta) {
 				current_beta = score;
--- ABCore.h
+++ ABCore.h
@@ -9,7 +9,13 @@ class PositionState;
 class PositionEvaluation;
 class TranspositionTable;
 
-const int16_t ASP_WINDOW = 1000;
+const uint16_t ASP_WINDOW = 40;
+const uint16_t DELTA = 20;
+const uint16_t ASP_DEPTH = 5;
+const uint16_t MAX_TRY = 5;
+
+const uint16_t MAX_QUIET_DEPTH = 10; 
+
 class ABCore
 {
 public:
@@ -26,7 +32,7 @@ public:
 private:
 	int16_t alpha_beta_iterative(PositionState& pos, uint16_t depth, int16_t alpha, int16_t beta, bool white_to_play);
 	int16_t alpha_beta(PositionState& pos, uint16_t depth, int16_t alpha, int16_t beta, bool white_to_play);
-	int16_t quiescence_search(PositionState& pos, int16_t alpha, int16_t beta, bool white_to_play);
+	int16_t quiescence_search(PositionState& pos, int16_t qs_depth, int16_t alpha, int16_t beta, bool white_to_play);
 	
 
 	ABCore(const ABCore&); // non-copyable
